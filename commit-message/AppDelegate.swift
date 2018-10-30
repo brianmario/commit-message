13e6f593c81435d25e4a1ae24a43f59c803ed9e1
Brian Lopez <seniorlopez@gmail.com>
2018-10-29 22:05:37 -0700

    Initial Commit

---
+++ commit-message/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  commit-message
+//
+//  Created by Brian Lopez on 10/29/18.
+//  Copyright © 2018 Brian Lopez. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ commit-message/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  commit-message
+//
+//  Created by Brian Lopez on 10/29/18.
+//  Copyright © 2018 Brian Lopez. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
